6e04caa4360152d4d45eac5b18a811bf2dc9d7a7
jstreifel <jstreifel@gmail.com>
2022-12-16 18:49:47 -0500

    Create Dev VPC fix

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ module "blog_sg" {
   vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingress-cidr_blocks = ["0.0.0.0/0"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
