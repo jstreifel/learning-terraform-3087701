0c00f5b8667b6948d68349c38408e7820481a800
jstreifel <jstreifel@gmail.com>
2022-12-17 21:50:35 -0500

    Create dev vpc copied from initial fail

--- main.tf
+++ main.tf
@@ -14,12 +14,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
-
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -50,16 +44,14 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.16.2"
-  name = "blog"
+  version = "4.13.0"
 
+  name = "blog"
   vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
-
 
