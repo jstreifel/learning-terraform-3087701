2853dfc3fecc32016311a079c84af946b74fb9ee
jstreifel <jstreifel@gmail.com>
2022-12-17 21:33:22 -0500

    test

--- main.tf
+++ main.tf
@@ -14,6 +14,12 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -31,7 +37,6 @@ module "vpc" {
   }
 }
 
-
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -46,12 +51,13 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.16.2"
-
   name = "blog"
+
   vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
+
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
