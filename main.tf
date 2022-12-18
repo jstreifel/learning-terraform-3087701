acc7c0401528617b7a4cbf614ba3b2fada33daa7
jstreifel <jstreifel@gmail.com>
2022-12-17 22:08:30 -0500

    going back again

--- main.tf
+++ main.tf
@@ -1,19 +1,22 @@
 data "aws_ami" "app_ami" {
   most_recent = true
+
   filter {
     name   = "name"
     values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
   }
+
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+
   owners = ["979382823631"] # Bitnami
 }
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  instance_type = var.instance_type
 
   tags = {
     Name = "HelloWorld"
