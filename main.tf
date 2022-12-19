37acd360f8f317223beb851e7a8ffcec04cc4058
jstreifel <jstreifel@gmail.com>
2022-12-19 01:50:45 -0500

    removed 16 from ingress blocks

--- main.tf
+++ main.tf
@@ -31,7 +31,16 @@ module "vpc" {
   }
 }
 
+resource "aws_instance" "blog" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  tags = {
+    Name = "Learning Terraform"
+  }
+}
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
@@ -41,7 +50,7 @@ module "blog_sg" {
   vpc_id = module.vpc.vpc_id
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/16"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
