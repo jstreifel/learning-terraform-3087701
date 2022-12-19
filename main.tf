a7ef3b76b2d3e35cfdaa989552771a573a130cfe
jstreifel <jstreifel@gmail.com>
2022-12-19 00:56:21 -0500

    changed ingress cidr blocks to 10.0.0.0/16 to match cidr of module vpc

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ module "blog_sg" {
   vpc_id = module.vpc.vpc_id
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_cidr_blocks = ["10.0.0.0/16"]
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
