db326d41a3d4259ba9c848d9772e6322eb2ce2a6
jstreifel <jstreifel@gmail.com>
2022-12-19 01:44:42 -0500

    changed public subnet to 16, from 24

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["10.0.101.0/16", "10.0.102.0/16", "10.0.103.0/16"]
 
   enable_nat_gateway = true
 
@@ -50,7 +50,7 @@ module "blog_sg" {
   vpc_id = module.vpc.vpc_id
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_cidr_blocks = ["0.0.0.0/16"]
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
