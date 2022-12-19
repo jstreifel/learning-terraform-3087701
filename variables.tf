2a4b2ebb2dd786350c1116f0582cea217d0f57ac
jstreifel <jstreifel@gmail.com>
2022-12-19 14:51:19 -0500

    fixed curly brackets?

--- variables.tf
+++ variables.tf
@@ -34,7 +34,7 @@ variable "environment" {
 variable "asg_min_size" {
   description "Minimum number of instances in the ASG"
   default = 1
-}
+ }
 
 variable "asg_max_size" {
   description "Maximum number of instances in the ASG"
