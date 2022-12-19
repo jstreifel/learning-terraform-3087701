85c06d7730ab74fc833652041266cc4f6465f032
jstreifel <jstreifel@gmail.com>
2022-12-19 02:01:21 -0500

    removed cidr from main.tf in line 37 and added to variables.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,6 @@ module "vpc" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  cidr = "10.0.0.0/16"
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
