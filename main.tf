759addedf169eaf60d4664edc6a913f11d6e1cbd
jstreifel <jstreifel@gmail.com>
2022-12-19 10:41:50 -0500

    changed module blog_vpc to vpc and changed the code in other pull line 39, 55, 56, 96blog_vpc

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "blog_vpc" {
+module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -36,7 +36,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  subnet_id = module.blog_vpc.public_subnets[0]
+  subnet_id = module.vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -52,8 +52,8 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
+  vpc_id             = module.vpc.vpc_id
+  subnets            = module.vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
   access_logs = {
@@ -93,7 +93,7 @@ module "blog_sg" {
   version = "4.16.2"
 
   name    = "blog_new"
-  vpc_id = module.blog_vpc.vpc_id
+  vpc_id = module.vpc.vpc_id
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
