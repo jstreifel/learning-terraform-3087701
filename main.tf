d88cff169c70317241dcb33e6b9fd439b71b0d5b
jstreifel <jstreifel@gmail.com>
2022-12-19 00:28:42 -0500

    Changed line 50 module.vpc.vpc_id

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ module "blog_sg" {
   version = "4.16.2"
 
   name    = "blog_new"
-  vpc_id = data.aws_vpc.default_vpc_id
+  vpc_id = module.vpc.vpc_id
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
