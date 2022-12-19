c0a1734f2cdf378ae9dbd9122786f432e339efa6
jstreifel <jstreifel@gmail.com>
2022-12-19 10:58:51 -0500

    autoscaling and alb

--- main.tf
+++ main.tf
@@ -27,24 +27,28 @@ module "vpc" {
   enable_dns_hostnames = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.7.0"
+  
+  name = "blog"
+  min_size = 1
+  max_soze = 2
 
-  subnet_id = module.vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
+  vpc_zone_identifier = module.vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  
+  image_id           = data.aws_ami.app_ami.id
+  instance_type      = var.instance_type
 }
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
@@ -68,8 +72,8 @@ module "alb" {
       target_type      = "instance"
       targets = {
         my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
+          target_id    = aws_instance.blog.id
+          port         = 80
         }
       }
     }
@@ -93,11 +97,11 @@ module "blog_sg" {
   version = "4.16.2"
 
   name    = "blog_new"
-  vpc_id = module.vpc.vpc_id
+  vpc_id  = module.vpc.vpc_id
 
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
