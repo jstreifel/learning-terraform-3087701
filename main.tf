9eaadc2e22c84f10b68e2bc7106150d94e2110c9
jstreifel <jstreifel@gmail.com>
2022-12-18 20:30:57 -0500

    Create Dev fix, indented line 38

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-vpc_security_group_ids = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "Learning Terraform"
@@ -45,8 +45,8 @@ vpc_security_group_ids = [module.blog_sg.security_group_id]
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.16.2"
-  name    = "blog_new"
 
+  name    = "blog_new"
   vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
